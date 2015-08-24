b6a514c5f079060fc2f6d3aab99be67f04960948
Matt Nohr <matt@smartthings.com>
2015-08-24 09:20:14 -0500

    Publishing first apps

---
+++ smartapps/mrnohr/turn-on-at.src/turn-on-at.groovy
@@ -0,0 +1,67 @@
+/**
+ *  Turn On At
+ *
+ *  Copyright 2013 Matt Nohr
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+/**
+ * Just turn on some lights at a given time, or when tapped
+ * Date: 2013-01-25
+ */
+definition(
+		name: "Turn On At",
+		namespace: "mrnohr",
+		author: "Matt Nohr",
+		description: "Turn on multiple lights at a given time",
+		category: "My Apps",
+		iconUrl: "https://dl.dropboxusercontent.com/u/2256790/smartapp-icons/clock.png",
+		iconX2Url: "https://dl.dropboxusercontent.com/u/2256790/smartapp-icons/clock%402x.png",
+		iconX3Url: "https://dl.dropboxusercontent.com/u/2256790/smartapp-icons/clock%402x.png")
+
+preferences {
+	section("At a specific time..") {
+		input "time1", "time", title: "When?"
+	}
+	section("Turn on some lights..") {
+		input "switches1", "capability.switch", multiple: true
+	}
+}
+
+def installed() {
+	log.debug "installed"
+	handleSchedule()
+}
+
+def updated() {
+	log.debug "updated"
+	unschedule()
+	handleSchedule()
+}
+
+def scheduleTurnOn() {
+	log.debug "turning on now"
+	turnOnLights()
+}
+
+def appTouch(evt) {
+	log.debug "appTouch: $evt"
+	turnOnLights()
+}
+
+private def handleSchedule() {
+	schedule(time1, "scheduleTurnOn")
+	subscribe(app)
+}
+
+private def turnOnLights() {
+	switches1?.on()
+}
